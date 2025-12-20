9cbde9736b8dcddd2309c0505cbbf8aa72bef84c
sandbuster2005 <dorengebenjamin@gmail.com>
2025-12-20 18:59:41 +0100

    created system to pull escape code of keys from infocmp and use them , currently pretty much working except maybe 1-2 name inversions

--- readchar/_posix_key.py
+++ readchar/_posix_key.py
@@ -1,50 +1,143 @@
 from ._base_key import *
-
-
 # common
 BACKSPACE = "\x7f"
+import subprocess
+import re
+import sys
+import logging
 
-# cursors
-UP = "\x1b\x5b\x41"
-DOWN = "\x1b\x5b\x42"
-LEFT = "\x1b\x5b\x44"
-RIGHT = "\x1b\x5b\x43"
+module = sys.modules[__name__]
 
-# navigation keys
-INSERT = "\x1b\x5b\x32\x7e"
-SUPR = "\x1b\x5b\x33\x7e"
-HOME = "\x1b\x5b\x48"
-END = "\x1b\x5b\x46"
-PAGE_UP = "\x1b\x5b\x35\x7e"
-PAGE_DOWN = "\x1b\x5b\x36\x7e"
+def _remove( text : str, chrs : list[str] ) -> str:
+    """
+    cette fonction permet de remplacer dans word les string contenu dans chr par new
+    """
+    return [text := f"".join( text.split( chraracter ) ) for chraracter  in chrs ][-1]
 
-# funcion keys
-F1 = "\x1b\x4f\x50"
-F2 = "\x1b\x4f\x51"
-F3 = "\x1b\x4f\x52"
-F4 = "\x1b\x4f\x53"
-F5 = "\x1b\x5b\x31\x35\x7e"
-F6 = "\x1b\x5b\x31\x37\x7e"
-F7 = "\x1b\x5b\x31\x38\x7e"
-F8 = "\x1b\x5b\x31\x39\x7e"
-F9 = "\x1b\x5b\x32\x30\x7e"
-F10 = "\x1b\x5b\x32\x31\x7e"
-F11 = "\x1b\x5b\x32\x33\x7e"
-F12 = "\x1b\x5b\x32\x34\x7e"
+name_correspondances = {
+    "ESC"                : "kbs",        #backspace key
+    "BEGIN"              : "kbeg",       #begin key
+    "CLEAR_ALL_TAB"      : "ktbc",       #clear-all-tabs key
+    "CLEAR"              : "kclr",       #clear-screen or erase key
+    "CLEAR_TAB"          : "kctab",      #clear-tab key
+    "BACK_TAB"           : "kcbt",       #back tab key
+    "ENTER"              : "kent",       #enter key
+    "DELETE"             : "kdch1",      #delete-character key
+    "UNkbown"            : "kdl1",       #delete-line key
+    "END"                : "kend",       #end key
+    "END_OF_LINE"        : "kel",        #clear-to-end-of-line key
+    "END_OF_SCREEN"      : "ked",        #clear-to-end-of-screen key
+    "HOME"               : "khome",      #home key
+    "INSERT_C"           : "kich1",      #insert-character key
+    "INSERT_LINE"        : "kil1",       #insert-line key
+    "HOME_DOWN"          : "kll",        #lower-left key (home down)
+    "PAGE_UP"            : "knp",        #next-page key
+    "PAGE_DOWN"          : "kpp",        #previous-page key
+    "SCROLL_FORWARD"     : "kind",       #scroll-forward key
+    "SCROLL_BACKWARD"    : "kri",        #scroll-backward key
+    "SET_TAB"            : "khts",       #set-tab key
+    "RIGHT"              : "kcuf1",      #right-arrow key
+    "LEFT"               : "kcub1",      #left-arrow key
+    "UP"                 : "kcuu1",      # up arrow key
+    "DOWN"               : "kcud1",      # down-arrow key
+    "PAD_UP_DOWN"        : "ka1",        #upper left of keypad
+    "PAD_UP_RIGHT"       : "ka3",        #upper right of keypad
+    "PAD_CENTER"         : "kb2",        #center of keypad
+    "PAD_DOWN_LEFT"      : "kc1",        #lower left of keypad
+    "PAD_DOWN_RIGHT"     : "kc3",        #lower right of keypad
+    "SHIFT_EXIT"         : "kEXT",       #shifted exit key
+    "SHIFT_FIND"         : "kFND",       #shifted find key
+    "SHIFT_HELP"         : "kHLP",       #shifted help key
+    "SHIFT_HOME"         : "kHOM",       #shifted home key
+    "SHIFT_INSERT_C"     : "kIC",        #shifted insert-character key
+    "SHIFT_LEFT"         : "kLFT",       #shifted left-arrow key
+    "SHIFT_RIGHT"        : "kRIT",       #shifted right-arrow key
+    "SHIFT_UP"           : "kUP",        #shifted up-arrow
+    "SHIFT_DOWN"         : "kDN",        #shifted down-arrow
+    "SHIFT_MESSAGE"      : "kMSG",       #shifted message key
+    "SHIFT_MOVE"         : "kMOV",       #shifted move key
+    "SHIFT_NEXT"         : "kNXT",       #shifted next key
+    "SHIFT_OPTIONS"      : "kOPT",       #shifted options key
+    "SHIFT_PREVIOUS"     : "kPRV",       #shifted previous key
+    "SHIFT_PRINT"        : "kPRT",       #shifted print key
+    "SHIFT_REDO"         : "kRDO",       #shifted redo key
+    "SHIFT_REPLACE"      : "kRPL",       #shifted replace key
+    "SHIFT_RESUME"       : "kRES",       #shifted resume key
+    "SHIFT_SAVE"         : "kSAV",       #shifted save key
+    "SHIFT_SUSPEND"      : "kSPD",       #shifted suspend key
+    "SHIFT_UNDO"         : "kUND",       #shifted undo key
+    "SHIFT_DELETE_LINE"  : "kDL",        #shifted delete line
+    "SHIFT_DELETE_C"     : "kDC",        #shifted delete chracter
+    "SHIFT_END"          : "kEND",       #shifted end key
+    "F0"                 : "kf0",        #F0 function key
+    "F1"                 : "kf1",        #F1 function key
+    "F2"                 : "kf2",        #F2 function key
+    "F3"                 : "kf3",        #F3 function key
+    "F4"                 : "kf4",        #F4 function key
+    "F5"                 : "kf5",        #F5 function key
+    "F6"                 : "kf6",        #F6 function key
+    "F7"                 : "kf7",        #F7 function key
+    "F8"                 : "kf8",        #F8 function key
+    "F9"                 : "kf9",        #F9 function key
+    "F10"                : "kf10",       #F10 function key
+    "F11"                : "kf11",       #F11 function key
+    "F12"                : "kf12",       #F12 function key
+    "F13"                : "kf13",       #F13 function key
+    "F14"                : "kf14",       #F14 function key
+    "F15"                : "kf15",       #F15 function key
+    "F16"                : "kf16",       #F16 function key
+    "F17"                : "kf17",       #F17 function key
+    "F18"                : "kf18",       #F18 function key
+    "F19"                : "kf19",       #F19 function key
+    "F20"                : "kf20",       #F20 function key
+    "F21"                : "kf21",       #F21 function key
+    "F22"                : "kf22",       #F22 function key
+    "F23"                : "kf23",       #F23 function key
+    "F24"                : "kf24"        #F24 function key
+}
+normal_mode = [
+    "UP",
+    "DOWN",
+    "LEFT",
+    "RIGHT",
+    "END",
+    "BEGIN",
+    "ENTER",
+    "HOME",
+    "PAD_CENTER",
+    "PAD_UP_DOWN",
+    "PAD_UP_RIGHT",
+    "PAD_DOWN_LEFT",
+    "PAD_DOWN_RIGHT"
+]
 
-# SHIFT+_
-SHIFT_TAB = "\x1b\x5b\x5a"
+data = subprocess.check_output(["infocmp","-x"])
+print(data)
+data = data.decode("utf-8")
+data = _remove(data , ["\n","\t"," "]) # new line tabs and spaces
+data = re.split("(?<=(?!\\\\).)," ,data) # split by "," and avoid "\,"
+keys = {}  # create dico with existing key
 
-# other
-CTRL_ALT_SUPR = "\x1b\x5b\x33\x5e"
+for x in range( len(data) ):
+    new_data = data[x].split("=")
+    if len(new_data) > 1 and new_data[0][0] == "k": # is a key and is attributed
+        keys[new_data[0]] = new_data[1]
 
-# ALT+_
-ALT_A = "\x1b\x61"
+for key in name_correspondances.keys():
 
-# CTRL+ALT+_
-CTRL_ALT_A = "\x1b\x01"
+    value = None
 
+    if name_correspondances[key] in keys.keys():
+        if key in normal_mode:
+            print(keys[name_correspondances[key]])
+            value = "\x1B" + "\x5B" + keys[name_correspondances[key]][3:] # terminals tend to be in normal mode and termnfo give app mode
+        else:
+            value = "\x1B" + keys[name_correspondances[key]][2:] # convert \E  to his code
 
-# aliases
+    setattr(module,key,value)
+
+    if not value:
+        logging.warn(f'{key} is not supported on this device')
+
 ENTER = LF
-DELETE = SUPR
+SUPR = DELETE
--- readchar/_posix_read.py
+++ readchar/_posix_read.py
@@ -67,19 +67,31 @@ class ReadChar:
         c1 = self.char()
         if c1 in self.config.INTERRUPT_KEYS:
             raise KeyboardInterrupt
+
         if c1 != "\x1B":
             return c1
+
         c2 = self.char()
         if c2 not in "\x4F\x5B":
             return c1 + c2
+
         c3 = self.char()
+
         if c3 not in "\x31\x32\x33\x35\x36":
             return c1 + c2 + c3
+
         c4 = self.char()
-        if c4 not in "\x30\x31\x33\x34\x35\x37\x38\x39":
+        if c4 not in "\x30\x31\x32\x33\x34\x35\x37\x38\x39\x3B":
             return c1 + c2 + c3 + c4
+
         c5 = self.char()
-        return c1 + c2 + c3 + c4 + c5
+        if c5 not in "\x30\x31\x32\x33\x34\x35\x37\x38\x39":
+            return c1 + c2 + c3 + c4 + c5
+
+        c6 = self.char()
+
+        return c1 + c2 + c3 + c4 + c5 + c6
+
 
 
 # Initially taken from:
@@ -110,7 +122,7 @@ def readkey() -> str:
     read and returned as noted in `_posix_key.py`."""
 
     c1 = readchar()
-
+    print(c1)
     if c1 in config.INTERRUPT_KEYS:
         raise KeyboardInterrupt
 
@@ -118,16 +130,25 @@ def readkey() -> str:
         return c1
 
     c2 = readchar()
+    print(c2)
     if c2 not in "\x4F\x5B":
         return c1 + c2
 
     c3 = readchar()
+    print(c3)
     if c3 not in "\x31\x32\x33\x35\x36":
         return c1 + c2 + c3
 
     c4 = readchar()
-    if c4 not in "\x30\x31\x33\x34\x35\x37\x38\x39":
+    print(c4)
+    if c4 not in "\x30\x31\x33\x34\x35\x37\x38\x39\x3b":
         return c1 + c2 + c3 + c4
 
     c5 = readchar()
-    return c1 + c2 + c3 + c4 + c5
+    print(c5)
+    if c4 not in "\x30\x31\x33\x34\x35\x37\x38\x39":
+        return c1 + c2 + c3 + c4 + c5
+
+    c6 = readchar()
+    print(c6)
+    return c1 + c2 + c3 + c4 + c5 + c6
